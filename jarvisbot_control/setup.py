8f903a82c1743f2fd34197d9a9e81d47f361931f
Deivaprakash <deivaprakash@goat-robotics.com>
2024-09-30 14:21:37 +0530

    packages addition

---
+++ jarvisbot_control/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'jarvisbot_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='deivaprakash',
+    maintainer_email='deivaprakash@goat-robotics.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'teleop_twist_keyboard = jarvisbot_control.teleop_twist_keyboard:main'
+        ],
+    },
+)
